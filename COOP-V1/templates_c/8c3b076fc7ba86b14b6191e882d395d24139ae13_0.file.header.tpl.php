5b9e43135538167852936329f85bec6ef7264abf
Thomas PARESCHI <thomas.pareschi@campus-centre.fr>
2023-09-07 20:25:24 +0200

    Instauration de l'accueil, presque terminé

--- COOP-V1/templates_c/8c3b076fc7ba86b14b6191e882d395d24139ae13_0.file.header.tpl.php
+++ COOP-V1/templates_c/8c3b076fc7ba86b14b6191e882d395d24139ae13_0.file.header.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.2, created on 2023-09-07 18:03:55
+/* Smarty version 4.3.2, created on 2023-09-07 18:24:46
   from 'C:\laragon\www\coop-emplois\COOP-V1\public\header.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.2',
-  'unifunc' => 'content_64fa108bca5d96_52158354',
+  'unifunc' => 'content_64fa156e1ec570_22525418',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '8c3b076fc7ba86b14b6191e882d395d24139ae13' => 
     array (
       0 => 'C:\\laragon\\www\\coop-emplois\\COOP-V1\\public\\header.tpl',
-      1 => 1694109834,
+      1 => 1694111074,
       2 => 'file',
     ),
   ),
@@ -20,42 +20,43 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_64fa108bca5d96_52158354 (Smarty_Internal_Template $_smarty_tpl) {
-?>       <!-- Header-->
-        <header id="header" class="header">
+function content_64fa156e1ec570_22525418 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-            <div class="header-menu">
+    <link rel="stylesheet" href="public/assets/css/style.css">
+    <title>Mon Site Web</title>
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <li><a href="/">Accueil</a></li>
+            <li><a href="/reunion.php">Réunion</a></li>
+            <li><a href="/entretiens.php">Entretiens</a></li>
+            <li><a href="/porteurs_de_projet.php">Porteurs de Projet</a></li>
+            <li><a href="/accompagnateurs.php">Accompagnateurs</a></li>
+            <li><a href="/parametres.php">Paramètres</a></li>
+            <li class="menu-item right"><a href="/plan_du_site.php">Plan du site</a></li>
+            <li class="menu-item right"><a href="/espace_personnel.php">Espace personnel</a></li>
+        </ul>
+    </nav>
+</header>
 
-                <div class="col-sm-7">
-                    <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a>
-                    <div class="header-left">
-                        <button class="search-trigger"><i class="fa fa-search"></i></button>
-                        <div class="form-inline">
-                            <form class="search-form">
-                                <input class="form-control mr-sm-2" type="text" placeholder="Search ..." aria-label="Search">
-                                <button class="search-close" type="submit"><i class="fa fa-close"></i></button>
-                            </form>
-                        </div>
+<main>
+    <h1>Contenu principal</h1>
+    <p>Bienvenue sur mon site web. Ceci est la page d'accueil.</p>
+</main>
 
-
-                    </div>
-                </div>
-
-                <div class="col-sm-5">
-                    <div class="user-area dropdown float-right">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <img class="user-avatar rounded-circle" src="public/images/admin.jpg" alt="User Avatar">
-                        </a>
-
-                    </div>
-                        <div class="user-area">
-                        Bienvenue Mr.XX
-                        </div>
-
-                </div>
-            </div>
-
-        </header><!-- /header -->
-        <!-- Header-->
+<footer>
+    <p>&copy; <?php echo '<?php'; ?>
+ echo date("Y"); <?php echo '?>'; ?>
+ Mon Site Web</p>
+</footer>
+</body>
+</html>
 <?php }
 }
