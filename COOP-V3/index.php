1f48c1b3ba7051f5fb337dc1ef5c4255212bff48
yuki_640 <640poto@gmail.com>
2023-09-08 16:29:11 +0200

    Accompagnateurs

---
+++ COOP-V3/index.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+require_once 'include/configuration.php';
+// gestion = ? client, produit, profil, accueil ?
+// action = ? lister (action par défaut), ajouter, modifier, supprimer, rechercher, trier, ...
+// action = form_ajouter (appel formulaire vierge), form_consulter (formulaire en consultation)
+
+// Existe GET pour rechercher et POST pour écriture
+// Mais aussi request
+Autoloader::chargerClasses();
+//if(!isset($_SESSION['login'])){
+//
+//    $_REQUEST['gestion'] = 'authentification';
+//
+/*else */if(!isset($_REQUEST['gestion'])){
+
+    $_REQUEST['gestion'] = 'accueil';
+
+}
+
+// Création d'un objet, instance de la classe de type (routeur) accueil (client, produit, ...)
+$oRouteur = new $_REQUEST['gestion']($_REQUEST);
+//var_dump($_REQUEST['gestion']);
+$oRouteur->choixAction();
