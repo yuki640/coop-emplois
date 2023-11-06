781f08123413e191ab5bf0ca51ede259c3403d81
samuel <640poto@gmail.com>
2023-11-06 17:05:55 +0100

    ok

--- COOP-VS.3/templates_c/7380c60d99fabee3d29c6de0adc02e4b9b3e4b58_0.file.accueilVue.tpl.php
+++ COOP-VS.3/templates_c/7380c60d99fabee3d29c6de0adc02e4b9b3e4b58_0.file.accueilVue.tpl.php
@@ -1,28 +1,28 @@
 <?php
-/* Smarty version 4.3.2, created on 2023-10-14 14:14:03
+/* Smarty version 4.3.2, created on 2023-10-15 11:34:13
   from 'C:\laragon\www\coop-emplois\COOP-VS.3\mod_accueil\vue\accueilVue.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
-if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array(
-    'version' => '4.3.2',
-    'unifunc' => 'content_652aa22b839323_53113300',
-    'has_nocache_code' => false,
-    'file_dependency' =>
-        array(
-            '7380c60d99fabee3d29c6de0adc02e4b9b3e4b58' =>
-                array(
-                    0 => 'C:\\laragon\\www\\coop-emplois\\COOP-VS.3\\mod_accueil\\vue\\accueilVue.tpl',
-                    1 => 1697042368,
-                    2 => 'file',
-                ),
-        ),
-    'includes' =>
-        array(
-            'file:public/header.tpl' => 1,
-            'file:public/footer.tpl' => 1,
-        ),
-), false)) {
-function content_652aa22b839323_53113300 (Smarty_Internal_Template $_smarty_tpl) {
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.2',
+  'unifunc' => 'content_652bce35765819_86436255',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '7380c60d99fabee3d29c6de0adc02e4b9b3e4b58' => 
+    array (
+      0 => 'C:\\laragon\\www\\coop-emplois\\COOP-VS.3\\mod_accueil\\vue\\accueilVue.tpl',
+      1 => 1697369485,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:public/header.tpl' => 1,
+    'file:public/footer.tpl' => 1,
+  ),
+),false)) {
+function content_652bce35765819_86436255 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html lang="fr">
 
@@ -39,19 +39,19 @@ function content_652aa22b839323_53113300 (Smarty_Internal_Template $_smarty_tpl)
 
 </head>
 <body>
-<?php $_smarty_tpl->_subTemplateRender('file:public/header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+    <?php $_smarty_tpl->_subTemplateRender('file:public/header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
-<div id="right-panel" class="right-panel">
+    <div id="right-panel" class="right-panel">
 
 
     <main>
-
-    <h1>Contenu principal</h1>
+    
+        <h1>Contenu principal</h1>
         <p>Bienvenue sur mon site web. Ceci est la page d'accueil.</p>
     </main>
 
-
+    
 </div><!-- /#right-panel -->
 
 <?php $_smarty_tpl->_subTemplateRender('file:public/footer.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
